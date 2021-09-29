44d41a144006876ad0d48c84e33416c6f01113a7
Oscar Korz <okorz001@gmail.com>
2021-09-28 17:58:41 -0700

    Close menu after selection

--- src/DropMenu.tsx
+++ src/DropMenu.tsx
@@ -20,6 +20,7 @@ export default function DropMenu(props: {
       <Menu
         anchorEl={anchorEl}
         open={anchorEl !== null}
+        onClick={hideMenu}
         onClose={hideMenu}
         PaperProps={{
           sx: {
